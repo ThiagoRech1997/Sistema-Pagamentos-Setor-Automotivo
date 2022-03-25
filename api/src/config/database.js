43e2a5de05de88e096c4d301513df94890371c14
Thiago Fernando Rech <thiagorech.1997@gmail.com>
2022-03-25 15:33:51 -0300

    #01 - update database connection

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'thiago',
-    password: 'cenwqg76',
-    database: 'dbautoteste',
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     define: {
         timestamps: true,
         underscored: true,
